4dd6d771fc0e1c655d4bfc995b023eabad273357
PolitieSluisDijk <72675542+PolitieSluisDijk@users.noreply.github.com>
2021-03-31 14:02:49 +0200

    Opdracht af

--- script.js
+++ script.js
@@ -32,53 +32,101 @@ $(function(){
 
     // Add cards to deck
     socials.forEach(function(social){
-        $("#memory").append('<i id="faceDown" class="fab ' + social + '"></i>')
+        $("#memory").append('<i class="fab ' + social + ' faceDown"></i>')
     });
 
-    // Create arrays
+    // Create arrays and variables
     let temporaryCards = []
-    let cardId = []
-    let correctCards = []
+    let correctCards = 0;
+    let incorrectTurns = 0;
+    let turns = 0;
 
     // Shows cards on click and adds to temporary array, after that it performs check
     $("#memory i").on("click", function(){
-        cardId.push($(this));
-        temporaryCards.push($(this).attr("class"));
-        if(temporaryCards.length < 2)
+        if(temporaryCards.length < 2 && $(this).hasClass("correct") != true && $(this).hasClass("faceUp") != true)
         {
-            $(this).attr("id", "faceUp");
+            $(this).removeClass("faceDown");
+            $(this).addClass("faceUp");
+            temporaryCards.push($(this).attr("class"));
         }
-        checkCards(temporaryCards, cardId);
+        if(temporaryCards.length == 2)
+        {
+            checkCards();
+            turns += 1;
+        }
+        update();
     });
 
-    // Checks card if correct, when correct displays otherwise hides again
-    function checkCards(cards, id){
-        if(cards.length == 2 && cards[0] == cards[1])
+    // Checks card if correct
+    function checkCards(){
+        if(temporaryCards[0] == temporaryCards[1])
         {
-            id[0].attr("id", "correct");
-            id[1].attr("id", "correct");
-            setTimeout(cardsCorrect(id[0], id[1]), 500)
+            $(".faceUp").addClass("correct");
+            $(".faceUp").removeClass("faceUp");
             temporaryCards = [];
+            correctCards += 2;
         }
-        else if(cards.length == 2)
+        else
         {
-            id[0].attr("id", "incorrect");
-            id[1].attr("id", "incorrect");
-            setTimeout(cardsIncorrect, 2000);
+            $(".faceUp").addClass("incorrect");
+            $(".faceUp").removeClass("faceUp");
+            setTimeout(cardsIncorrect, 2000)
+            incorrectTurns += 1;
             temporaryCards = [];
         }
     };
 
-    function cardsIncorrect(card1, card2){
-        console.log(card1);
-        card1.attr("id", "faceDown");
-        card2.attr("id", "faceDown");
-        cardId = [];
-    };
+    function cardsIncorrect(){
+        $(".incorrect").addClass("faceDown");
+        $(".incorrect").removeClass("incorrect");
+    }
 
-    function cardsCorrect(){
-        correctCards.push(cards[0]);
-        correctCards.push(cards[1]);
-        cardId = [];
-    };
+    function update(){
+        // Displays amount of incorrect turns
+        if(incorrectTurns > 0)
+        {
+            $("#fouten").text(incorrectTurns);
+        }
+
+        // Displays amount of total turns
+        if(turns > 0)
+        {
+            $("#turns").text(turns);
+        }
+        
+        // Performs actions when all cards are correct
+        if(correctCards == 16)
+        {
+            finished()
+        }
+
+        // Removes 1 star after 16 wrong turns
+        if(incorrectTurns > 16)
+        {
+            $("#star-3").css("color", "grey");
+        }
+
+        // Removes 2 star after 24 wrong turns
+        if(incorrectTurns > 24)
+        {
+            $("#star-2").css("color", "grey");
+        }
+
+        // Removes 3 star after 24 wrong turns
+        if(incorrectTurns > 32)
+        {
+            $("#star-1").css("color", "grey");
+        }
+    }
+
+    // Function that performs when you finish te game
+    function finished(){
+        $("#body").css("opacity", "0.6")
+        $("#body").fireworks();
+    }
+
+    // Restarts the game
+    $("#restart").click(function(){
+        location.reload();
+    })
 })
